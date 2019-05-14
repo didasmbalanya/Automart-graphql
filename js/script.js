5c14d413ed7bc7af660fe14556f8cd6ddcc51127
Didas Mbalanya <didasmbalanya@gmail.com>
2019-05-14 18:12:02 +0300

    add about and fix bug on login functionality

--- js/script.js
+++ js/script.js
@@ -6,3 +6,7 @@ function hide(element){
 function unhide(element){
     document.querySelector(element).style.display = 'inherit';
 };
+
+function bgUnhide(element){
+    document.querySelector(element).style.display = 'flex';
+};
