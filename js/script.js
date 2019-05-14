78214bfce37fad82c7d1638d3b4e3a68aee0c42d
Didas Mbalanya <didasmbalanya@gmail.com>
2019-05-14 03:51:31 +0300

    [finishes #165938228] adds sign up modal form

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function signUp() {
+    document.querySelector('.bg-modal').style.display = 'flex';
+}
+
+function closeBox(){
+    document.querySelector('.bg-modal').style.display = 'none';
+}
+
+
