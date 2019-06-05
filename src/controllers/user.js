db41acdfe37fa27871b359de07d2d9eaaee232ec
Didas Mbalanya <didasmbalanya@gmail.com>
2019-06-05 16:16:02 +0300

    [finishes #166415543]add jwt to sign un api

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -2,11 +2,13 @@
 /* eslint-disable consistent-return */
 import Joi from '@hapi/joi';
 import bcrtypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 import { signUnSchema, users } from '../models/user';
+
 
 export const signup = (req, res) => {
   Joi.validate(req.body, signUnSchema).then(() => {
-    const { email, password } = req.body;
+    const { email, password, id } = req.body;
     const foundUser = users.find(user => user.email === email);
     if (foundUser) {
       return res.status(422).send('already signed up with email address');
@@ -14,8 +16,10 @@ export const signup = (req, res) => {
     req.body.id = users.length + 1;
     req.body.password = bcrtypt.hashSync(password, 8);
     req.body.confirm_password = bcrtypt.hashSync(password, 8);
+    const token = jwt.sign({ id: req.body.id }, 'mysecret');
+    const user = req.body;
     users.push(req.body);
-    res.status(201).send(req.body);
+    res.status(201).send({ user, token });
   }).catch((e) => {
     res.status(422).send(e.details[0].message);
   });
