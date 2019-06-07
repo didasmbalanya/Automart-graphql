5dd6da8c3f50e53e7f5d12ac2cd43b72e8ddc5db
Didas Mbalanya <didasmbalanya@gmail.com>
2019-06-07 10:57:05 +0300

    [finishes #166465716] add post car api and functionality

--- src/controllers/car.js
+++ src/controllers/car.js
@@ -1,19 +1,29 @@
 /* eslint-disable linebreak-style */
+/* eslint-disable consistent-return */
 import Joi from '@hapi/joi';
 import { cars, carSchema } from '../models/car';
 
 export const postCar = (req, res) => {
   Joi.validate(req.body, carSchema).then(() => {
-    res.send(req.body);
-  }).catch();
+    const car = req.body;
+    car.id = cars.length + 1;
+    car.created_on = Date();
+    cars.push(car);
+    res.send(car);
+  }).catch((e) => {
+    if (e.details[0].message) {
+      res.status(422).send(e.details[0].message);
+    } else {
+      res.status(404).send('Invalid post request');
+    }
+  });
 };
 
 export const getCars = (req, res) => {
   if (cars.length >= 1) {
-    res.status(200).send(cars);
-  } else {
-    res.send(404).send('cars not found');
+    return res.status(200).send(cars);
   }
+  res.send(404).send('cars not found');
 };
 
 export const getCarById = (req, res) => {
--- src/models/car.js
+++ src/models/car.js
@@ -7,8 +7,8 @@ export const cars = [
     id: 1,
     owner: 1, // user id
     created_on: 'DateTime',
-    state: 'String', // new,used
-    status: 'String', // sold,available - default is available
+    state: 'new', // new,used
+    status: 'available', // sold,available - default is available
     price: 'Float',
     manufacturer: 'String',
     model: 'String',
@@ -17,7 +17,6 @@ export const cars = [
 
 ];
 
-const availableCars = cars.map(car => car.model);
 export const carSchema = Joi.object().keys({
   state: Joi.string().valid(['new', 'used']).required(),
   status: Joi.string().valid(['sold', 'available']).default('available'),
