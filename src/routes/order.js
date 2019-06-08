3113620972bac137fd1e3c3da0927b558ca7376f
Didas Mbalanya <didasmbalanya@gmail.com>
2019-06-08 18:41:14 +0300

    add auth to order api-endpoints

--- src/routes/order.js
+++ src/routes/order.js
@@ -1,12 +1,13 @@
 /* eslint-disable linebreak-style */
 import express from 'express';
 import { postOrder, updateOrder, getOrderById } from '../controllers/order';
+import { auth } from '../middleware/auth';
 
 const router = express.Router();
 
 
-router.post('/api/v1/order', postOrder);
-router.patch('/api/v1/order/:id', updateOrder);
-router.get('/api/v1/order/:id', getOrderById);
+router.post('/api/v1/order', auth, postOrder);
+router.patch('/api/v1/order/:id', auth, updateOrder);
+router.get('/api/v1/order/:id', auth, getOrderById);
 
 export default router;
